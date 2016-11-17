aead8705f3ae7641658b68da3e617a9964bfa058
Senthil.G.R <sgundepa@cisco.com>
2016-11-17 21:01:22 +0530

    phantom

--- phantompdf.js
+++ phantompdf.js
@@ -1,10 +1,20 @@
 var page = require('webpage').create();
 var system = require('system');
 var args = system.args;
-console.log(args);
-console.log(args[0],args[1],args[2]);
-page.open(args[2], function(status) {
-  console.log("pdf created---------->");
-  page.render(args[1]+".pdf");
-  phantom.exit();
-});
+page.open(args[2]);
+page.viewportSize = { width: 1080, height: 800 };
+page.paperSize = {
+  width: '600px',
+  height: '300px',
+  margin: {
+    top: '0px',
+    left: '0px',
+    bottom:'0px',
+    right:'0px'
+  }
+};
+page.onLoadFinished = function(status) {    
+      page.render(args[1]+".pdf",{format: 'pdf',quality: '100'});
+      phantom.exit();
+
+};
